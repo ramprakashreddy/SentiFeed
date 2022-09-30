6e898f19b4b4c4a3e091f2f98dcf7b35b19065b9
atimabh <abarunaha@gmail.com>
2022-10-01 01:11:11 +0530

    fix: home ui

--- App.js
+++ App.js
@@ -1,45 +1,4 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Image,
-  Text,
-  StatusBar,
-} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from "./src/Home";
-import Graph from "./src/Graph";
 
-
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#dbf0f7"
-          }
-        }}
-      >
-        <Stack.Screen name="HomeScreen" component={Home}
-          options={{
-            title: "",
-            headerLeft: () => (
-              <View style={{ marginLeft: 20 }}>
-                <Image
-                  source={require("./src/images/Header.png")}
-                  style={{ width: 180, height: 30 }}
-                />
-              </View>
-            ),
-
-          }}
-        />
 
         <Stack.Screen name="HomeScreen" component={Home}
           options={{
